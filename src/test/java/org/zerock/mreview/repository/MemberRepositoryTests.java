c959dd1df271b35bbd89c74c0d13d7db55cbc406
kwangho kwak <678hoa@gmail.com>
2021-08-06 19:59:39 +0900

    Delete 관련 테스트코드 추가 및 Repository 추가.

--- src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -2,6 +2,8 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.entity.Member;
 import java.util.stream.IntStream;
 
@@ -11,11 +13,28 @@ public class MemberRepositoryTests {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private ReviewRepository reviewRepository;
+
     @Test
     public void insertMember() {
         IntStream.rangeClosed(1,100).forEach(i -> {
             Member member = Member.builder().email("r"+i+"rr.rrr").pw("11111111").nickname("reviewer"+i).build();
             memberRepository.save(member);
         });
+    }
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+        Long mid = 1L;
+
+        Member member = Member.builder().mid(mid).build();
+
+        //memberRepository.deleteById(mid);
+        //reviewRepository.deleteByMember(member);
+
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
     }
 }
--- src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -1,19 +1,23 @@
 package org.zerock.mreview.repository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
-import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Member;
+import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
+
     @Autowired
     private ReviewRepository reviewRepository;
+
 
 
     @Test
@@ -39,4 +43,23 @@ public class ReviewRepositoryTests {
             reviewRepository.save(movieReview);
         });
     }
+
+
+    @Test
+    public void testGetMovieReviews() {
+
+        Movie movie = Movie.builder().mno(92L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+
+            System.out.print(movieReview.getReviewnum());
+            System.out.print("\t"+movieReview.getGrade());
+            System.out.print("\t"+movieReview.getText());
+            System.out.print("\t"+movieReview.getMember().getEmail());
+            System.out.println("---------------------------");
+        });
+    }
+
 }
