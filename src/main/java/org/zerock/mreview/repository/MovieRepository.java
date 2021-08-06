ca78cf18f1c8de5bc7ccc5d0eb76e46c619ead46
kwangho kwak <678hoa@gmail.com>
2021-08-06 19:17:00 +0900

    ReviewRepository 내용 추가.

--- src/main/java/org/zerock/mreview/repository/MovieRepository.java
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -20,7 +20,7 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
 
 
     @Query("select m, mi, avg(coalesce(r.grade,0)), count(r) " +
-            " from Movie m left outer join MovieImage mi on mi.movie = m " +
+            "from Movie m left outer join MovieImage mi on mi.movie = m " +
             "left outer join Review r on r.movie =m" +
             " where m.mno = :mno group by mi")
     List<Object[]> getMovieWithAll(Long mno);
