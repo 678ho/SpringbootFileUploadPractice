071ea74b9675b390a55d8a1a3592ccdef660f217
kwangho kwak <678hoa@gmail.com>
2021-08-16 13:33:06 +0900

    오탈자 수정 및 누락된 내용 추가.

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -1,17 +1,26 @@
 package org.zerock.mreview.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequestMapping("/movie")
 @Log4j2
+@RequiredArgsConstructor
 public class MovieController {
+
 
     @GetMapping("/register")
     public void register() {
 
     }
+
 }
