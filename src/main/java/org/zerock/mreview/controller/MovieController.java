bae442c6bac12590b9779bf827fd4b5a2313591e
kwangho kwak <678hoa@gmail.com>
2021-08-17 14:14:08 +0900

    MovieServiceImpl은 MovieController에서는 POST방식으로 전달된 파라미터들을 MovieDTO로 수집해서 MovieService타입 객체의 register 호출하도록 작성.

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.service.MovieService;
 
 
 @Controller
@@ -17,10 +19,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class MovieController {
 
+    private final MovieService movieService;
 
     @GetMapping("/register")
     public void register() {
 
+    }
+
+    @PostMapping
+    public String register(MovieDTO movieDTO, RedirectAttributes redirectAttributes) {
+        log.info("movieDTO : " + movieDTO);
+
+        Long mno = movieService.register(movieDTO);
+
+        redirectAttributes.addFlashAttribute("msg", mno);
+
+        return "redirect:/movie/list";
     }
 
 }
