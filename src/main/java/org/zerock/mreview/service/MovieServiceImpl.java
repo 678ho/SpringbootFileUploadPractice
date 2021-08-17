47de91b95e2be6a2c805ad449864d1576c39bb6e
kwangho kwak <678hoa@gmail.com>
2021-08-17 14:02:26 +0900

    MovieServiceImpl은 MovieRepository와 MovieImageRepository를 주입받도록 구성하고, dtoToEntity에서 반환한 객체들을 이용해서 세이브함.

--- src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
+++ src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
@@ -4,8 +4,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.entity.Movie;
+import org.zerock.mreview.entity.MovieImage;
+import org.zerock.mreview.repository.MovieImageRepository;
 import org.zerock.mreview.repository.MovieRepository;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 @Log4j2
@@ -14,8 +21,21 @@ public class MovieServiceImpl implements MovieService{
 
     private final MovieRepository movieRepository;
 
+    private final MovieImageRepository imageRepository;
+
+    @Transactional
     @Override
     public Long register(MovieDTO movieDTO){
-        return null;
+
+        Map<String, Object> entityMap = dtoToEntity(movieDTO);
+        Movie movie = (Movie) entityMap.get("movie");
+        List<MovieImage> movieImageList = (List<MovieImage>) entityMap.get("imgList");
+
+        movieRepository.save(movie);
+
+        movieImageList.forEach(movieImage -> {
+            imageRepository.save(movieImage);
+        });
+        return movie.getMno();
     }
 }
