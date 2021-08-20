0c61884880fe41ecba521a899bcf28123c872f52
kwangho kwak <678hoa@gmail.com>
2021-08-20 14:52:08 +0900

    영화의 번호를 이용해서 MovieDTO를 반환하는 기능을 정의하고 MovieServiceImpl에서 구현함.

--- src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
+++ src/main/java/org/zerock/mreview/service/MovieServiceImpl.java
@@ -15,6 +15,7 @@ import org.zerock.mreview.entity.MovieImage;
 import org.zerock.mreview.repository.MovieImageRepository;
 import org.zerock.mreview.repository.MovieRepository;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -49,6 +50,26 @@ public class MovieServiceImpl implements MovieService{
         });
 
         return movie.getMno();
+    }
+
+    @Override
+    public MovieDTO getMovie(Long mno) {
+
+        List<Object[]> result = movieRepository.getMovieWithAll(mno);
+
+        Movie movie = (Movie) result.get(0)[0];
+
+        List<MovieImage> movieImageList = new ArrayList<>();
+
+        result.forEach(arr -> {
+            MovieImage movieImage = (MovieImage)arr[1];
+            movieImageList.add(movieImage);
+        });
+
+        Double avg = (Double) result.get(0)[2];
+        Long reviewCnt = (Long) result.get(0)[3];
+
+        return entitiesToDTO(movie, movieImageList, avg, reviewCnt);
     }
 
     @Override
