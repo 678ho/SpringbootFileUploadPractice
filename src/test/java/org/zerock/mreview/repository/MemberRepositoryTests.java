75cffb300a7e99866a21a7e6bc9d945d29c8ba22
kwangho kwak <678hoa@gmail.com>
2021-08-23 19:02:44 +0900

    회원 엔티티클래스를 처리하기 위한 클래스 구현

--- src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -1,15 +1,17 @@
 package org.zerock.mreview.repository;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.entity.Member;
-import java.util.stream.IntStream;
 
+import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemberRepositoryTests {
+
     @Autowired
     private MemberRepository memberRepository;
 
@@ -17,22 +19,27 @@ public class MemberRepositoryTests {
     private ReviewRepository reviewRepository;
 
     @Test
-    public void insertMember() {
+    public void insertMembers() {
+
         IntStream.rangeClosed(1,100).forEach(i -> {
-            Member member = Member.builder().email("r"+i+"rr.rrr").pw("11111111").nickname("reviewer"+i).build();
+            Member member = Member.builder()
+                    .email("r" + i + "@zerock.org")
+                    .pw("1111")
+                    .nickname("reviewer" + i)
+                    .build();
+
             memberRepository.save(member);
         });
     }
+
     @Commit
     @Transactional
     @Test
     public void testDeleteMember() {
+
         Long mid = 1L;
 
         Member member = Member.builder().mid(mid).build();
-
-        //memberRepository.deleteById(mid);
-        //reviewRepository.deleteByMember(member);
 
         reviewRepository.deleteByMember(member);
         memberRepository.deleteById(mid);
