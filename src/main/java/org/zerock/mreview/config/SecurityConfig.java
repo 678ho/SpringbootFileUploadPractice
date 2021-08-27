f911a6e011f0e9219cf0a325ad269a467e010c95
kwangho kwak <678hoa@gmail.com>
2021-08-27 11:30:22 +0900

    AuthenticationManager(암호화된 패스워드를 사용하기위해 해당 암호를 사용하는 사용자 설정.)

--- src/main/java/org/zerock/mreview/config/SecurityConfig.java
+++ src/main/java/org/zerock/mreview/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package org.zerock.mreview.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$nIG8EDZl1HfXOMVL.EqVAuZClma7JRqgooLPs3Nn/aF3atbhj7pDG")
+                .roles("USER");
     }
 
 }
