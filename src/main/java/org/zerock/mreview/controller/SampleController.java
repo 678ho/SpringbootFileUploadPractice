e43b9d857483f7f5eb4502802eb1a9991bec5c66
kwangho kwak <678hoa@gmail.com>
2021-08-29 20:02:47 +0900

    컨트롤러에서 사용자 정보를 확인처리를 위한 로직 추가.

--- src/main/java/org/zerock/mreview/controller/SampleController.java
+++ src/main/java/org/zerock/mreview/controller/SampleController.java
@@ -2,9 +2,11 @@ package org.zerock.mreview.controller;
 
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.mreview.security.dto.ClubAuthMemberDTO;
 
 @Controller
 @Log4j2
@@ -17,8 +19,11 @@ public class SampleController {
     }
 
     @GetMapping("/member")
-    public void exMember() {
-        log.info("member......");
+    public void exMember(@AuthenticationPrincipal ClubAuthMemberDTO clubAuthMemberDTO) {
+        log.info("exMember");
+
+        log.info("-------------------------------------------");
+        log.info(clubAuthMemberDTO);
     }
 
     @GetMapping("/admin")
