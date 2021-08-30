5b022fabf7220025f762b004dd1ebf9c92adaaa8
kwangho kwak <678hoa@gmail.com>
2021-08-30 12:12:20 +0900

    소셜로그인한 회원의 정보 로그 추출 및 자동 회원가입처리.

--- src/main/java/org/zerock/mreview/security/service/ClubUserDetailsService.java
+++ src/main/java/org/zerock/mreview/security/service/ClubUserDetailsService.java
@@ -18,38 +18,40 @@ import java.util.stream.Collectors;
 @Log4j2
 @Service
 @RequiredArgsConstructor
-public class ClubUserDetailsService implements UserDetailsService {
-
+public class ClubUserDetailsService  implements UserDetailsService {
 
     private final ClubMemberRepository clubMemberRepository;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
         log.info("ClubUserDetailsService loadUserByUsername " + username);
+
 
         Optional<ClubMember> result = clubMemberRepository.findByEmail(username, false);
 
-        if(result.isPresent()) {
-            throw new UsernameNotFoundException("Check Email or Social");
+        if(result.isEmpty()){
+            throw new UsernameNotFoundException("Check User Email or from Social ");
         }
 
         ClubMember clubMember = result.get();
 
-        log.info("--------------------");
+        log.info("-----------------------------");
         log.info(clubMember);
 
-        ClubAuthMemberDTO clubAuthMemberDTO = new ClubAuthMemberDTO(
-                clubMember.getName(),
+        ClubAuthMemberDTO clubAuthMember = new ClubAuthMemberDTO(
+                clubMember.getEmail(),
                 clubMember.getPassword(),
                 clubMember.isFromSocial(),
-                clubMember.getRoleSet().stream().map( role ->
-                        new SimpleGrantedAuthority("ROLE_"+role.name())).collect(Collectors.toSet())
+                clubMember.getRoleSet().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_"+role.name()))
+                        .collect(Collectors.toSet())
         );
-        clubAuthMemberDTO.setName(clubMember.getName());
-        clubAuthMemberDTO.setFromSocial(clubMember.isFromSocial());
 
-        return clubAuthMemberDTO;
+        clubAuthMember.setName(clubMember.getName());
+
+
+        return clubAuthMember;
     }
 }
 /*
